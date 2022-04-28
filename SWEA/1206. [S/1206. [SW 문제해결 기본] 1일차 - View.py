5eb6d2dfcb18b65e64f02d8a05313517683df2ac
hjle2 <02q09q@gmail.com>
2022-04-28 23:25:57 +0900

    1206

---
+++ SWEA/1206. [S/1206. [SW 문제해결 기본] 1일차 - View.py
@@ -0,0 +1,16 @@
+# 왼, 오 창문을 열었을 때 양쪽 모두 2 이상! 의 공간
+# 맨 왼쪽 두칸과 오른쪾 두칸은 건물이 없다
+# 최대 높이는 255
+def sunlight():
+    ans = 0
+    for i in range(2, n-2):
+        h = a[i]
+        other = max(a[i-2],a[i-1],a[i+1],a[i+2])
+        ans += h-other if h>other else 0
+    return ans
+
+
+for t in range(10):
+    n = int(input())
+    a = list(map(int, input().split()))
+    print(f'#{t+1}', sunlight())
