faf77f6c822834950480abac6602b209ab1a3667
hjle2 <02q09q@gmail.com>
2022-06-27 01:31:35 +0900

    DP 방법! for 구문이 핵심!

--- 단기간 성장/11049_행렬 곱셈 순서.py
+++ 단기간 성장/11049_행렬 곱셈 순서.py
@@ -1,18 +1,43 @@
-def dfs(s, e, cnt):
-    if s == 0 and e == N-1:
-        global ans
-        ans = min(ans, cnt)
-        return
-    if s-1 >= 0:
-        dfs(s-1, e, cnt + rc[s-1][0] * rc[s-1][1] * rc[e][1])
-    if e+1 < N:
-        dfs(s, e+1, cnt + rc[s][0] * rc[e+1][0] * rc[e+1][1])
+def solve(s, e):
+    ret = 0
+    if s == e:
+        return ret
+    if dp[s][e]:
+        return dp[s][e]
+    if s+1 == e:
+        ret = rc[s][0] * rc[s][1] * rc[e][1]
+        dp[s][e] = ret
+        return ret
+
+    for i in range(s, e):
+        ret = solve(s, i) + solve(i+1, e) + rc[s][0] * rc[i][1] * rc[e][1]
+        if not dp[s][e] or ret < dp[s][e]:
+            dp[s][e] = ret
+    return dp[s][e]
 
 
-ans = 0
 N = int(input())
 rc = [[*map(int, input().split())]for _ in range(N)]
-for i in range(N-1):
-    dfs(i, i+1, rc[i][0] * rc[i][1] * rc[i+1][1])
-print(ans)
+dp = [[0]*N for _ in range(N)]
+print(solve(0, N-1))
 
+
+# 1. DFS 방식
+# def dfs(s, e, cnt):
+#     if s == 0 and e == N-1:
+#         global ans
+#         ans = min(ans, cnt)
+#         return
+#     if s-1 >= 0:
+#         dfs(s-1, e, cnt + rc[s-1][0] * rc[s-1][1] * rc[e][1])
+#     if e+1 < N:
+#         dfs(s, e+1, cnt + rc[s][0] * rc[e+1][0] * rc[e+1][1])
+#
+#
+# ans = 0
+# N = int(input())
+# rc = [[*map(int, input().split())]for _ in range(N)]
+# for i in range(N-1):
+#     dfs(i, i+1, rc[i][0] * rc[i][1] * rc[i+1][1])
+# print(ans)
+#
