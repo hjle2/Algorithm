990662bd92b3e7f7bc18204cce1d1cd41d30a4c7
hjle2 <02q09q@gmail.com>
2022-07-01 22:59:32 +0900

    zip 사용하는 방법 short code 에서 찾음 python 으로 통과하려면 이방법 밖에 없는듯..?

--- DP 최적화/11049_행렬 곱셈 순서.py
+++ DP 최적화/11049_행렬 곱셈 순서.py
@@ -1,10 +1,24 @@
+# N = int(input())
+# lst = [[*map(int, input().split())] for _ in range(N)]
+# dp = [[0]*N for _ in range(N+1)]
+#
+# for d in range(1, N):
+#     for s in range(N - d):
+#         e = s + d
+#         dp[s][e], ot[s] = min((dp[s][s+i] + dp[s+i+1][e] + lst[s][0] * lst[e][1] * lst[s+i][1], i)for i in range(ot[s], ot[e]))
+# print(dp[0][-1])
+
+# zip short code 에서 찾음
 N = int(input())
-lst = [[*map(int, input().split())] for _ in range(N)]
-dp = [[0]*N for _ in range(N+1)]
-ot = [*range(N)]
+dp_size = [0] * (N+1)
+dp = [[0]*N for _ in range(N)]
+for i in range(N-1):
+    dp_size[i] = next(map(int, input().split()))
+dp_size[N-1], dp_size[N] = map(int, input().split())
 
 for d in range(1, N):
     for s in range(N - d):
         e = s + d
-        dp[s][e], ot[s] = min((dp[s][s+i] + dp[s+i+1][e] + lst[s][0] * lst[e][1] * lst[s+i][1], i)for i in range(ot[s], ot[e]))
-print(dp[0][-1])
+        size = dp_size[s] * dp_size[e+1]
+        dp[s][e] = min(size*s + f + b for s, f, b in zip(dp_size[s+1:e+1], dp[s][s:e], dp[e][s+1:e+1]))
+print(dp[0][N-1])
