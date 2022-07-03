95ca45f64434f867172d2edf2402f0bf233e7c41
hjle2 <02q09q@gmail.com>
2022-07-04 03:12:29 +0900

    쉽게 통과!

--- 분할 정복/11729_하노이 탑 이동 순서.py
+++ 분할 정복/11729_하노이 탑 이동 순서.py
@@ -3,26 +3,18 @@
 # 항상 위의 것이 아래 것 보다 작다.
 # 위의 규칙을 따라 첫번째 장대에서 세번째 장대로 옮겨야 한다.
 # 이동 횟수의 최소 값을 구하라
-def next(frm, to):
-    for i in [1, 2, 3]:
-        if i != frm and i != to:
-            return i
-
 
 def solve(n, frm, to):
     if n == 1:
         print(frm, to)
         return
-    solve(n-1, frm, next(frm, to))
+    solve(n-1, frm, 6-frm-to)
     print(frm, to)
-    solve(n-1, next(frm, to), to)
+    solve(n-1, 6-frm-to, to)
 
 
 # N 1<=N<=20
 N = int(input())
-v = 1
-for _ in range(1, N):
-    v = v * 2 + 1
 # 가장 큰 N 원판을 제외한 모든 원판을 3이 아닌 2로 옮겨야 함!
-print(v)
+print(2**N-1)
 solve(N, 1, 3)
