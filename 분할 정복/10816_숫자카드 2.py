738b8c1db2335aa33a2f6d308e06da75d97c2dd4
hjle2 <02q09q@gmail.com>
2022-07-04 02:07:27 +0900

    Counter 로 시간초과 문제가 해결되었다!

--- 분할 정복/10816_숫자카드 2.py
+++ 분할 정복/10816_숫자카드 2.py
@@ -1,18 +1,13 @@
 # 보유한 숫자카드 N 개 중에서 정수 M 개 에 대해 몇 개 갖고 있는지 구하라
-def Count(x, i):
-    l = r = i
-    while l >= 0 and cards[l] == x:
-        l -= 1
-    while r < N and cards[r] == x:
-        r += 1
-    return r-l -1
+from collections import Counter
+
 
 def find(x):
     l, r = 0, N-1
     while l <= r:
         m = (l+r)//2
         if cards[m] == x:
-            return Count(x, m)
+            return counter[x]
         if cards[m] < x:
             l =m+1
         else:
@@ -22,6 +17,7 @@ def find(x):
 
 N = int(input())
 cards = [*sorted(map(int, input().split()))]
+counter = Counter(cards)
 M = int(input())
 nums = [*map(int, input().split())]
 
