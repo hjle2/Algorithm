a73600eb680074214a08f95b81d69f2b7b8441b2
hjle2 <02q09q@gmail.com>
2022-07-05 06:09:54 +0900

    골드 1 되니까 갑자기 확 어려워졌네.. 가로, 세로 방향으로 자를 수 있음. 외곽, 결정체, 아무것도 없는 것은 자를 수 없음. 1. 라인을 자르는 것으로 생각해야 함. 2. 결정체가 1개, 불순물이 1개면 잘못 자른 것!!! --> 불순물을 제거하기위해 자르면 한쪽은 결정체가 1개, 나머지 한쪽은 결정체가 없는 조각이 되기 때문이다!!!

---
+++ 분할 정복/2339_석판 자르기.py
@@ -0,0 +1,59 @@
+def isok(x0, y0, x1, y1):
+    s = j = 0
+    for x in range(x0, x1):
+        for y in range(y0, y1):
+            if A[x][y] == 1:
+                s += 1
+            elif A[x][y] == 2:
+                j += 1
+    if j == 1:
+        if s == 0:
+            return 1
+        else:
+            return 0
+    if s == 0 or j == 0:
+        return 0
+    return -1
+
+
+def cancut(x0, y0, x1, y1, n, d):
+    if x0 + 1 == x1 or y0 + 1 == y1:
+        return 0
+    rg = [range(x0, x1), range(y0, y1)][d]
+    s = 0
+    for i in rg:
+        x, y = [n, i][not d], [n, i][d]
+        if A[x][y] == 1:
+            s += 1
+        elif A[x][y] == 2:
+            return 0
+    return s
+
+
+def solve(x0, y0, x1, y1, d):
+    ok = isok(x0, y0, x1, y1)
+    if ok >= 0:
+        return ok
+    ret, d = 0, not d
+    rg = [range(x0, x1), range(y0, y1)][not d]
+    for i in rg:
+        if cancut(x0, y0, x1, y1, i, d):
+            a = solve(x0, y0, [x1, i][d], [i, y1][d], d)
+            if a == 0: continue
+            b = solve([x0, i+1][d], [i+1, y0][d], x1, y1, d)
+            if b == 0: continue
+            ret += a * b
+    return ret
+
+
+N = int(input())
+A = [[*map(int, input().split())]for _ in range(N)]
+
+# while True:
+#     a, b, c, d, e, f = map(int, input().split())
+#     print(cancut(a, b, c, d, e, f))
+#     if a == 0 and c == 0:
+#         break
+
+ans = solve(0, 0, N, N, 0) + solve(0, 0, N, N, 1)
+print(ans if ans > 0 else -1)
