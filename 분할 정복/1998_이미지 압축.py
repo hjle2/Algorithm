6b0462429953879d7480b427c4425c4567e65fb0
hjle2 <02q09q@gmail.com>
2022-07-10 23:57:02 +0900

    unhashable type 문제를 해결하면 되지 않을까..?

--- 분할 정복/1998_이미지 압축.py
+++ 분할 정복/1998_이미지 압축.py
@@ -1,38 +1,53 @@
+def isall(x, y, n):
+    for i in range(n):
+        for j in range(n):
+            if A[x+i][y+j] != A[x][y]:
+                return False
+    return True
+
+
+def isinmem(x, y, n):
+    tmp = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            tmp[i][j] = A[x+i][y+j]
+    if tmp in mem:
+        return True
+    else:
+        mem.add(tmp)
+        return False
+
+
 def init():
     big = max(R, C)
     n = 1
     while n < big:
         n <<= 1
-    if n-R > 0:
-        for i in range(R):
-            for _ in range(n-C):
-                A[i].append(0)
-    for _ in range(n-R):
-        A.append([0] * n)
+    for i in range(R):
+        for j in range(n-C):
+            A[i].append(0)
+    for i in range(n-R):
+        A.append([0]*n)
     return n
-
-
-def isall(x, y, n):
-    for r in range(n):
-        for c in range(n):
-            if A[x+r][y+c] != A[x][y]:
-                return False
-    return True
 
 
 def solve(x, y, n):
     if isall(x, y, n):
-        return 1
-    if n == 2:
-        return 4
-    ret = 1
-    for dx, dy in d:
-        nx, ny = x + dx*n, y + dy*n
-        ret += solve(x, y, n // 2)
-    return ret
+        if isinmem(x, y, n):
+            return 1, 0
+        else:
+            return 1, 1
+    og, ef = 1, 1
+    n //= 2
+    for dx, dy in D:
+        a, b = solve(x + dx*n, y + dy*n, n)
+        og, ef = og+a, ef+b
+    return og, ef
 
-d = [(0, 0), (0, 1), (1, 0), (1, 1)]
+
+D = [(0, 0), (0, 1), (1, 0), (1, 1)]
 R, C = map(int, input().split())
-A = [[*input()]for _ in range(R)]
+A = [[*map(int, [*input()])]for _ in range(R)]
 N = init()
-print(solve(0, 0, N))
+mem = set()
+print(*solve(0, 0, N))
