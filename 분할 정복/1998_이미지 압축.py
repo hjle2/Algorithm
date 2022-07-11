ef6dc844fde3caaf09f0dc98d30c969e6c37ccd6
hjle2 <02q09q@gmail.com>
2022-07-11 21:26:46 +0900

    통과 됬는데..모르겠음 ^^

--- 분할 정복/1998_이미지 압축.py
+++ 분할 정복/1998_이미지 압축.py
@@ -1,21 +1,8 @@
-def isall(x, y, n):
-    for i in range(n):
-        for j in range(n):
-            if A[x+i][y+j] != A[x][y]:
-                return False
-    return True
-
-
-def isinmem(x, y, n):
-    tmp = [[0]*n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            tmp[i][j] = A[x+i][y+j]
-    if tmp in mem:
-        return True
-    else:
-        mem.add(tmp)
-        return False
+def hashFunc(str):
+    ret = 0
+    for i in range(len(str)):
+        ret = (ret * 32741 + ord(str[i])) % 1e9+7
+    return ret
 
 
 def init():
@@ -32,22 +19,49 @@ def init():
 
 
 def solve(x, y, n):
-    if isall(x, y, n):
-        if isinmem(x, y, n):
-            return 1, 0
-        else:
-            return 1, 1
-    og, ef = 1, 1
-    n //= 2
+    allsame = True
+    tmp = []
+    for i in range(n):
+        for j in range(n):
+            tmp.append(A[x+i][y+j])
+            if A[x][y] != A[x + i][y + j]:
+                allsame = False
+
+    if allsame:
+        isleaf[x][y][n] = 1
+        qStr[x][y][n] = "W" if A[x][y] else "B"
+        return 1
+
+    ret = 1
+    s = n//2
     for dx, dy in D:
-        a, b = solve(x + dx*n, y + dy*n, n)
-        og, ef = og+a, ef+b
-    return og, ef
+        ret += solve(x + dx * s, y + dy * s, s)
+        qStr[x][y][n] += (qStr[x+dx*s][y+dy*s][s])
+    return ret
+
+
+def eff(x, y, n):
+    if isleaf[x][y][n]:
+        return 1
+    if hashFunc(qStr[x][y][n]) in mp.keys():
+        return 0
+    mp[hashFunc(qStr[x][y][n])] = 1
+
+    ret = 1
+    s = n//2
+    for dx, dy in D:
+        ret += eff(x + dx*s, y + dy*s, s)
+    return ret
 
 
 D = [(0, 0), (0, 1), (1, 0), (1, 1)]
 R, C = map(int, input().split())
 A = [[*map(int, [*input()])]for _ in range(R)]
+mp = dict()
 N = init()
-mem = set()
-print(*solve(0, 0, N))
+isleaf = [[[0]*(N+1) for _ in range(N)]for _ in range(N)]
+qStr = [[["X"]*(N+1) for _ in range(N)]for _ in range(N)]
+ans = [0, 0]
+ans[0] = solve(0, 0, N)
+ans[1] = eff(0, 0, N)
+print(*ans)
