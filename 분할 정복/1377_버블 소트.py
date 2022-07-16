e625819d335acb59cd82b01493124d67ad5ba305
hjle2 <02q09q@gmail.com>
2022-07-17 00:23:57 +0900

    숏코딩에서 배운 것! 1. enumerate 를 사용하면 for 문에서 index 를 쉽게 얻을 수 있다! (알 고 있었지만, 문제 풀면서 까먹었는데 다시 되새기기!) 2. sorted([j, i] for i, j in E(arr)) 방법으로 [j, i]에 대해서 sort한 배열을 얻어 반복문에서 탐색할 수 있다! 이런 방법으로도 sorted 배열을 얻을 수 있는 줄 몰랐음!! sort 하여 하나의 배열을 생성하는 것 보다 sorted 로 정렬된 배열을 얻어 반복문에서 사용하고, 원래의 배열을 다시 탐색할 때 아주 유용할 듯싶다!

--- 분할 정복/1377_버블 소트.py
+++ 분할 정복/1377_버블 소트.py
@@ -1,4 +1,21 @@
+# N = int(input())
+# A = [[int(input()), i] for i in range(N)]
+# A.sort()
+# print(max(A[i][1]-i for i in range(N))+1)
+
+
+# 숏 코딩 파이썬으로 통과
 N = int(input())
-A = [[int(input()), i] for i in range(N)]
-A.sort()
-print(max(A[i][1]-i for i in range(N))+1)
+A = [int(input())for _ in range(N)]
+# 와 이걸 몰랐네..
+# 배열을 반복문에서 순회할 때, 변수를 하나 추가하면 인덱스를 확인 할 수 있따..
+# 그리고 이거를 [i, j] 로 sorted 를 걸어주면..!
+# 이렇게 배열이 된다...!!!!
+# sorted[j, i] for i, j in enumerate(A)
+
+# enumerate !!
+# enumerate(배열) 은
+# 인덱스, 배열의 인덱스 값!!!!
+E = enumerate
+
+print(max(j[1]-i for i, j in E(sorted([j, i]for i,j in E(A))))+1)
