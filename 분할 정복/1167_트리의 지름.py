a8831844a8f25b3d9ea8039c6240f079693dbd0b
hjle2 <02q09q@gmail.com>
2022-07-18 02:32:31 +0900

    오호.... line 을 초기화 하는 과정에서 error 가 발생된거 같다.. ㅜㅜ 근데 1. 왜 첫번째 dfs 후에 visited 를 초기화해줘야 하는지 2.defaultdict 를 어떻게, 어떨 때 사용하는 건지 확인이 필요하다!!!!

--- 분할 정복/1167_트리의 지름.py
+++ 분할 정복/1167_트리의 지름.py
@@ -1,31 +1,47 @@
-# import sys
-# input = sys.stdin.readline
-#
-# def dfs(node, sum):
-#     visited[node] = True
-#     global diam
-#     global far_node
-#     if diam < sum:
-#         diam, far_node = sum, node
-#
-#     for i in range(0, len(line[node]), 2):
-#         next_node, distance = line[node][i:i+2]
-#         print(next_node, distance)
-#         if not visited[next_node]:
-#             dfs(next_node, sum + distance)
-#
-#
-# N = int(input()) + 1
-# line = [[]for _ in range(N)]
-# for i in range(1, N):
-#     line[i] = [*map(int, input().split())][1:-1]
-#
-# far_node = diam = 0
-# visited = [False] * N
-# dfs(1, 0)
-# dfs(far_node, diam)
-# print(diam)
+import sys
 from collections import defaultdict
 
-graph = defaultdict(list)
-print(graph)
+input = sys.stdin.readline
+def dfs(node, d):
+    global far_node
+    global diam
+    visited[node] = True
+    if d > diam:
+        far_node, diam = node, d
+    # for u, c in graph[node]:
+    #     if not visited[u]:
+    #         dfs(u, d + c)
+    # for i in range(0, len(line[node]), 2):
+    #     nxt, dst = line[node][i:i+2]
+    #     if not visited[nxt]:
+    #         dfs(nxt, d + dst)
+    for u, c in line[node]:
+        if not visited[u]:
+            dfs(u, d + c)
+
+N = int(input()) + 1
+line = [[]for _ in range(N)]
+
+for i in range(N-1):
+    tmp = [*map(int, input().split())]
+    key = tmp[0]
+    for j in range(1, len(tmp)-1, 2):
+        line[key].append(tmp[j:j+2])
+
+# for i in range(1, N):
+#     line[i] = [*map(int, input().split())][1:-1]
+
+# graph = defaultdict(list)
+# for _ in range(N-1):
+#     temp = list(map(int,input().split()))
+#     key = temp[0]
+#     for i in range(1,len(temp)-1,2):
+#         graph[key].append((temp[i],temp[i+1]))
+
+visited = [False] * N
+far_node = diam = 0
+dfs(1, 0)
+visited = [False] * N
+dfs(far_node, 0)
+print(diam)
+
