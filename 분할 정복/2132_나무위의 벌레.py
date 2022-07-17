4c05ba3b17a80a2ab7ca3b57bb38104545484999
hjle2 <02q09q@gmail.com>
2022-07-18 00:55:56 +0900

    트리의 지름? 을 구해야 한다고 하는데 뭔소린지 하나도 모르겠음 ^^

--- 분할 정복/2132_나무위의 벌레.py
+++ 분할 정복/2132_나무위의 벌레.py
@@ -1,16 +1,5 @@
 import sys
 
-def dfs(que, v):
-    now = que[-1]
-    ret = v
-    for i in A[now]:
-        if not visited[i]:
-            visited[i] = True
-            ret = max(ret, dfs(que + [i], v + C[i]))
-            visited[i] = False
-    return ret
-
-
 input = sys.stdin.readline
 N = int(input())
 C = [*map(int, input().split())]
@@ -20,16 +9,3 @@ for i in range(N-1):
     A[a-1].append(b-1)
     A[b-1].append(a-1)
 
-visited = [False] * N
-s = sum(C)
-ans = idx = 0
-for i in range(N):
-    if A[i]:
-        visited[i] = True
-        tmp = dfs([i], C[i])
-        if tmp > ans:
-            ans, idx = tmp, i
-        if ans == s:
-            break
-        visited[i] = False
-print(ans, idx+1)
