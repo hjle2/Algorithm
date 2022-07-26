feaafeaa14be3dbdfdeace9b2c3fc01998912a29
hjle2 <02q09q@gmail.com>
2022-07-27 01:10:37 +0900

    와.. 이런 방법으로 비교해서 DP 푸는 것도 있었구나.. 다 까먹었다..

--- 동적 계획법 1/9251_LCS.py
+++ 동적 계획법 1/9251_LCS.py
@@ -1,8 +1,8 @@
 A, B = [*input()], [*input()]
 len_A, len_B = len(A)+1, len(B)+1
 dp = [[0] * len_A for _ in range(len_B)]
+# A의 i 까지, B의 j까지 비교했을 때, 가장 긴 것
 for i in range(1, len_B):
     for j in range(1, len_A):
-        # !!
         dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1] + (A[j-1] == B[i-1]))
-print(dp[len_B-1][len_A-1])
+print(dp[-1][-1])
