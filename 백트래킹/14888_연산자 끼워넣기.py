2b80985dbae2a67fff33cd85ded81c95a15c4958
hjle2 <02q09q@gmail.com>
2022-07-27 00:35:49 +0900

    C++14 기준으로 음수 나누기를 하는 게.. 까다롭고 쉬움.

---
+++ 백트래킹/14888_연산자 끼워넣기.py
@@ -0,0 +1,34 @@
+def operate(a, op, b):
+    if op == 0:
+        return a+b
+    elif op == 1:
+        return a-b
+    elif op == 2:
+        return a*b
+    else:
+        if a >= 0:
+            return a//b
+        else:
+            return (-a)//b * -1
+
+
+def dfs(n, s):
+    global minA, maxA
+    if n == N:
+        minA = min(minA, s)
+        maxA = max(maxA, s)
+        return
+    for i in range(4):
+        if op[i]:
+            op[i] -= 1
+            dfs(n+1, operate(s, i, A[n]))
+            op[i] += 1
+
+
+N = int(input())
+A = [*map(int, input().split())]
+op = [*map(int, input().split())]
+minA, maxA = 1e9, -1e9
+dfs(1, A[0])
+print(maxA, minA, sep='\n')
+
