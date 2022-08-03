f3fd03257ffe369eed62d31b84d7c101a9d04a07
WOW <62990408+hjle2@users.noreply.github.com>
2022-08-03 12:14:19 +0900

    Update 10942_팰린드롬?.py
    팰린드롬 탐색
    (다이나믹 프로그래밍 방법을 이용하여 가장 가!까!운 노드를 탐색하여 최단 거리를 구하는 다익스트라 알고리즘)

--- 단기간 성장/10942_팰린드롬?.py
+++ 단기간 성장/10942_팰린드롬?.py
@@ -1,38 +1,25 @@
-# 홍준이가 적은 N개의 자연수
-# 명우에게 질문 M번
 import sys
 
 input = sys.stdin.readline
 
-dp = []
-nums = []
-def getDPVal(i, j):
-    if i == 0: return 1
-    if nums[j] != nums[j+i]:
-        return 0
-    if i == 1:
-        return 1
-    return dp[i-2][j+1]
-
-def makeDP(N):
-    global dp
-    dp = [[0] * N for _ in range(N)]
+def bellman_ford():
+    dist[1] = 0
     for i in range(N):
-        for j in range(N-i):
-            dp[i][j] = getDPVal(i, j)
+        for a, b, c in busLine:
+            if dist[a] != INF and dist[b] > dist[a] + c:
+                if i == N-1:
+                    return True
+                dist[b] = dist[a] + c
+    return False
 
-
-def solve():
-    global nums
-    N = int(input())
-    nums = [*map(int, input().split())]
-    M = int(input())
-    makeDP(N, nums, M)
-
-    for i in range(M):
-        s, e = map(int, input().split())
-        print(dp[e-s][s-1])
-
-
-if __name__ == "__main__":
-    solve()
+INF = 1e9
+N, M = map(int, input().split())
+busLine = []
+for _ in range(M):
+    busLine.append([*map(int, input().split())])
+dist = [INF] * (N+1)
+if bellman_ford():
+    print(-1)
+else:
+    for i in range(2, N+1):
+        print(dist[i] if dist[i] < INF else -1)
