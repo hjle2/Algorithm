dcda5e27ec8a7b94602833b5670a14a7ef7205dd
hjle2 <02q09q@gmail.com>
2022-08-06 20:37:24 +0900

    ** 누적 합의 나머지 합 ** 1. 수학적으로 공식 도출 2. Cnt 배열을 이용하여 갯!수!만 따로 저장 3. 0부터 시작하는 구간은 따로 더해줌 4. 조합 공식을 이용하여 경우의 수만 계산!!

--- 누적 합/10986_나머지 합.py
+++ 누적 합/10986_나머지 합.py
@@ -1,26 +1,20 @@
 import sys
-
 input = sys.stdin.readline
+
 N, M = map(int, input().split())
 ar = [*map(int, input().split())]
-# 와 사람들 천재 많다....
-# 배열의 구간 연속 합은 sum[j] - sum[i]
-# 이 문제는 연속 합의 나머지가 0인 i, j 쌍의 갯수를 구해야 한다
-# 여기에서 (sum[j] - sum[i]) % M == 0 인 i, j쌍은
-# sum[j] % M - sum[i] % M == 0
-# sum[j] % M == sum[i] % M 인 i, j 쌍의 갯수를 구해야 한다. !! 이 부분이 포인트
-
 sum = 0
+# sum[i] % M == sum[j] % M 인 i, j의 갯수를 찾아라
 cnt = [0] * M
+# 각 나머지가 index 인 구간의 갯수
 for i in range(N):
     sum += ar[i]
     sum %= M
     cnt[sum] += 1
 
 ans = cnt[0]
+# 0 부터 시작하는 구간 중에 나머지가 0인 구간 포함!
+# nC2 = n ! // 2 // (n-2) -> n * n-1 / 2
 for i in range(M):
-    # cnt[i] = sum[i]%M 이 v인 i 의 갯수 n
-    # 이 중에 두개를 뽑아 i, j 쌍을 만드는 경우의 수를 구한다!
     ans += cnt[i] * (cnt[i]-1) // 2
-
 print(ans)
