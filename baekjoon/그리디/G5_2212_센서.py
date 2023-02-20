729705616b38d0f62f4332a6119f7b9e5378adec
hjle <02q09q@naver.com>
2023-02-20 23:20:15 +0900

    G5 Greedy 센서

--- baekjoon/그리디/G5_2212_센서.py
+++ baekjoon/그리디/G5_2212_센서.py
@@ -1,21 +1,15 @@
-# 고속도로위에 N개의 센서가 있다
-# 고속도로 위에 K개의 집중국을 세울 수 있다
-# 집중국은 수신 가능 영역을 조절할 수 있는데, 고속도로 상 연결된 구간으로 나타난다
-# N개의 센서가 적어도 하나의 집중국과 통신해야 한다
-# 집중국의 수신 가능 영역 길이의 합을 최소화한 값을 구하라
 def solve():
-    if K >= N:
-        return 0
-    dist = []
-    for i in range(1, N):
-        # 센서 i 와 센서 i-1 사이의 거리
-        dist.append(sensors[i] - sensors[i-1])
-    dist.sort()
-    for _ in range(K-1):
-        dist.pop()
-    return sum(dist)
+    if n <= k: return 0 # 설치할 집중국이 모든 센서에 설치할 수 있을만큼 충분하다면 모든 집중국은 수신 가능 영역의 길이가 0이다
 
-N = int(input())
-K = int(input())
+    dist = [0] * (n-1)
+    for i in range(n-1):
+        dist[i] = sensors[i+1] - sensors[i] # 모든 센서들의 수신 사이 거리를 구한다
+
+    dist.sort(key=lambda x:-x) # 수신사이 거리가 긴 순으로 정렬하여 그 둘을 한번에 수신하지 않도록 분리한다
+    return sum(dist[k-1:]) # k개 설치할 수 있다면 k개의 영역으로 쪼개어 k-1개의 가장 긴 거리를 수신하지 않도록 분리시킨다.
+
+n = int(input())
+k = int(input())
+
 sensors = sorted([*map(int, input().split())])
 print(solve())
