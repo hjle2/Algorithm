29fd12e1ee17b3b0fa2dea78196c7411aac7e92d
hjle <02q09q@naver.com>
2023-02-21 21:18:13 +0900

    G5 Greedy 배

--- baekjoon/그리디/G5_1092_배.py
+++ baekjoon/그리디/G5_1092_배.py
@@ -1,56 +1,29 @@
-# 박스안에 있는 화물을 배에 실어야 한다
-# 항구에는 크레인이 N대 있고, 1분에 1개의 박스를 배에 실을 수 있다
-# 모든 크레인은 동시에 움직인다
-# 크레인에는 무게 제한이 있다
-# 모든 박스를 배로 옮기는 데 드는 시간의 최솟값을 구하여라
-
 import sys
 
 input = sys.stdin.readline
 
 
-def getAns():
-    W.sort()
-    B.sort()
-    if W[-1] < B[-1]:
+def solve():
+    if crain[0] < box[0]: # 크레인이 들지 못하는 박스가 있으면 -1
         return -1
 
-    cnt = [0] * N
-    wi = 0
-    for bi, b in enumerate(B):
-        if b <= W[wi]:
-            cnt[wi] += 1
-        else:
-            while W[wi] < b:
-                wi += 1
-            cnt[wi] += 1
     t = 0
-    total = 0
-    while 1:
+    while box:
         t += 1
-        for wi, w in enumerate(W):
-            if cnt[wi]:
-                cnt[wi] -= 1
-                total += 1
-            else:
-                while wi >= 0 and cnt[wi] == 0:
-                    wi -= 1
-                if wi >= 0:
-                    cnt[wi] -= 1
-                    total += 1
-
-        if total == M:
-            break
+        for c in crain: # 크레인을 돌면서
+            if not box or c < box[-1]: break # 크레인을 계속 탐색한다고 하더라도 옮길 수 있는 박스가 없다면 더 탐색하지 않는다.
+            for bi, b in enumerate(box): # 옮길 수 있는 박스를 찾는다
+                if c >= b: # 박스를 옮길 수 있따면
+                    del box[bi] # 옮길 박스에서 지우고 박스 탐색 그만하기!
+                    break
     return t
 
-N = int(input())
-W = [*map(int, input().split())]
-M = int(input())
-B = [*map(int, input().split())]
 
-print(getAns())
-# 4
-# 2 3 5 10
-# 17
-# 1 1     1 1 1 2 2       4 5 5 5 5          9 9 9 9 9
-# -> 6
+n = int(input()) # 50
+crain = [*map(int, input().split())]
+m = int(input()) # 10000
+box = [*map(int, input().split())]
+crain.sort(reverse=True)
+box.sort(reverse=True)
+
+print(solve())
