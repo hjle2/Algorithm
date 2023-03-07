04080f63e94216a7485ffd392bf525fcc456434e
WOW <62990408+hjle2@users.noreply.github.com>
2023-03-07 13:18:36 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,11 @@
+import sys
+si = sys.stdin.readline
+
+n = int(input())
+a = sorted([*map(int, input().split())])
+b = sorted([*map(int, input().split())], reverse=True)
+
+ans = 0
+for i in range(n):
+    ans += a[i] * b[i]
+print(ans)
