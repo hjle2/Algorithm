c90d7aab233b2ffdc33f5578ae5230f26b76977e
WOW <62990408+hjle2@users.noreply.github.com>
2023-03-07 14:41:50 +0900

    [Gold IV] Title: 단어 수학, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.py
+++ 백준/Gold/1339. 단어 수학/단어 수학.py
@@ -14,21 +14,30 @@ for num in nums:
         cnt[ord(c) - oa] += digit
         digit //= 10
 
-for v in range(9, -1, -1):
-    max_idx = -1
-    max_cnt = 0
-    for i, c in enumerate(cnt):
-        if max_cnt < c:
-            max_idx, max_cnt = i, c
-    if max_idx == -1:
-        break
-    cnt[max_idx] = -1
-    val[max_idx] = v
+# for v in range(9, -1, -1):
+#     max_idx = -1
+#     max_cnt = 0
+#     for i, c in enumerate(cnt):
+#         if max_cnt < c:
+#             max_idx, max_cnt = i, c
+#     if max_idx == -1:
+#         break
+#     cnt[max_idx] = -1
+#     val[max_idx] = v
+#
+# ans = 0
+# for num in nums:
+#     digit = 10 ** (len(num)-1)
+#     for c in num:
+#         ans += val[ord(c) - oa] * digit
+#         digit //= 10
+# print(ans)
 
+cnt.sort(reverse=True)
+v = 9
 ans = 0
-for num in nums:
-    digit = 10 ** (len(num)-1)
-    for c in num:
-        ans += val[ord(c) - oa] * digit
-        digit //= 10
+for c in cnt:
+    if not c: break
+    ans += c * v
+    v -= 1
 print(ans)
