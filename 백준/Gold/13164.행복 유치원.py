ee733c4a0cff180988d07dbb61765d73ba0fd959
WOW <62990408+hjle2@users.noreply.github.com>
2023-03-31 22:30:10 +0900

    Create 13164.행복 유치원.py

---
+++ 백준/Gold/13164.행복 유치원.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+nums = sorted([*map(int, input().split())])
+
+sub = []
+for i in range(n-1):
+    sub.append(nums[i+1] - nums[i])
+sub.sort()
+for i in range(k-1):
+    sub.pop()
+print(sum(sub))
