e2cbae2a51914deb8e9fe875b36978fad610b92d
WOW <62990408+hjle2@users.noreply.github.com>
2023-04-13 21:27:30 +0900

    [Gold II] Title: 보석 도둑, Time: 1576 ms, Memory: 120312 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -46,6 +46,8 @@ public class Main {
 
             if (!steal.isEmpty()) {
                 ans += steal.poll();
+            } else if (j == n) {
+                break;
             }
         }
 
