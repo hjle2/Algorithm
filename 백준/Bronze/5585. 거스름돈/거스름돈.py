792869f666935e9be611e3a0c0476e42b5e3f5bd
WOW <62990408+hjle2@users.noreply.github.com>
2023-04-16 21:57:10 +0900

    [Bronze II] Title: 거스름돈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,9 +1,22 @@
-n = 1000 - int(input())
-coin = [500, 100, 50, 10, 5, 1]
+n = int(input())
+n = 1000 - n
 
-cnt = 0
-for i in coin:
-    if n >= i:
-        cnt += n // i
-        n = n % i
-print(cnt)
+ans = 0
+if n >= 500:
+    ans += 1
+    n -= 500
+if n >= 100:
+    ans += n // 100
+    n = n % 100
+if n >= 50:
+    ans += 1
+    n -= 50
+if n >= 10:
+    ans += n // 10
+    n = n % 10
+if n >= 5:
+    ans += 1
+    n -= 5
+if n > 0:
+    ans += n
+print(ans)
