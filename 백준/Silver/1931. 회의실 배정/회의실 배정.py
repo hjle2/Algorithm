f05bd9d656588cebea702757119d8053ebedd307
WOW <62990408+hjle2@users.noreply.github.com>
2023-04-18 22:01:03 +0900

    [Silver I] Title: 회의실 배정, Time: 4936 ms, Memory: 59204 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,12 +1,12 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
+meeting = [[*map(int, input().split())]for _ in range(n)]
+meeting.sort(key=lambda x:(x[1], x[0]))
 
-N = int(input())
-timetable = [[*map(int, input().split())]for _ in range(N)]
-timetable.sort(key=lambda x: (x[1], x[0]))
-cnt = now = 0
-for s, e in timetable:
-    if s >= now:
-        cnt+=1
-        now = e
-print(cnt)
+prv = 0
+ans = 0
+for i in range(n):
+    s, e = meeting[i]
+    if s < prv: continue
+    ans += 1
+    prv = e
+print(ans)
