80ea4d097b209975621b1022c06961edc84e9bdc
WOW <62990408+hjle2@users.noreply.github.com>
2023-04-25 20:52:00 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,10 +1,12 @@
-N = int(input())
-ans = 0
-if N in [4, 7]:
-    print(-1)
-    exit()
-while N % 5:
-    ans += 1
-    N -= 3
-ans += N//5
-print(ans)
+n = int(input())
+
+def solve(n):
+    if n == 4 or n == 7:
+        return -1
+    ans = 0
+    while n % 5:
+        ans += 1
+        n -= 3
+    return ans + n // 5
+
+print(solve(n))
