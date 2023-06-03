941ca35d92b66bb55ccb60b5ebbdfd98bf6806f5
WOW <62990408+hjle2@users.noreply.github.com>
2023-06-03 16:08:56 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 288 ms, Memory: 46956 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -0,0 +1,34 @@
+def get_ans():
+    ans = 0
+    stack = []
+
+    for i, cur_h in enumerate(h):
+
+        if stack and stack[-1][1] > cur_h:
+            while stack and stack[-1][1] > cur_h:
+                tmp_i, tmp_h = stack.pop()
+                width = i
+                if stack:
+                    width = i - 1 - stack[-1][0]
+                square = width * tmp_h
+                ans = max(ans, square)
+
+        if not stack or stack[-1][1] <= cur_h:
+            stack.append((i, cur_h))
+
+    i = n
+    while stack:
+        tmp_i, tmp_h = stack.pop()
+        width = i
+        if stack:
+            width = i - 1 - stack[-1][0]
+        square = width * tmp_h
+        ans = max(ans, square)
+    return ans
+
+
+while True:
+    n, *h = map(int, input().split())
+    if n == 0:
+        break
+    print(get_ans())
