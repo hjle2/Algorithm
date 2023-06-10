388ac39d173d10b87b35a54c5428503ed7ec14b6
WOW <62990408+hjle2@users.noreply.github.com>
2023-06-11 00:04:14 +0900

    Update 가장 긴 증가하는 부분 수열 2.py

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -6,15 +6,15 @@ LIS = [0]
 for num in arr:
     if LIS[-1] < num:
         LIS.append(num)
-    else:
+    else:   # 증가하는 숫자가 아니라면, 현재 숫자가 들어갈 위치를 이분 탐색으로 찾는다.
         left = 0
         right = len(LIS) - 1
 
-        while left < right:
+        while left <= right:
             mid = (left + right) // 2
             if LIS[mid] < num:
                 left = mid + 1
             else:
-                right = mid
-        LIS[right] = num
+                right = mid-1 # mid + 1 이 아닌 mid 값을 할당하기 때문에 while문의 조건에 등호가 빠져야 한다.
+        LIS[right+1] = num
 print(len(LIS) - 1)
