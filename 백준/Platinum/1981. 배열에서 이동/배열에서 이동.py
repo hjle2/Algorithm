4ef6b125761814da85a18371d378c294d6b971dc
WOW <62990408+hjle2@users.noreply.github.com>
2023-06-10 21:09:16 +0900

    Update 배열에서 이동.py

--- 백준/Platinum/1981. 배열에서 이동/배열에서 이동.py
+++ 백준/Platinum/1981. 배열에서 이동/배열에서 이동.py
@@ -3,6 +3,12 @@ from collections import deque
 n = int(input())
 board = [[*map(int, input().split())] for _ in range(n)]
 
+# 배열 내부의 가장 작은 값, 큰 값 구하기
+min_v, max_v = 1e9, 0
+for i in range(n):
+    min_v = min(min_v, min(board[i]))
+    max_v = max(max_v, max(board[i]))
+
 
 # 최대, 최소 값이 범위 내로 n-1, n-1에 도착할 수 있으면 1을 반환 아니라면 0을 반환하는 함수
 def bfs(left, right):
@@ -23,21 +29,14 @@ def bfs(left, right):
     return False
 
 
-# 배열 내부의 가장 작은 값, 큰 값 구하기
-min_v, max_v = 1e9, 0
-for i in range(n):
-    min_v = min(min_v, min(board[i]))
-    max_v = max(max_v, max(board[i]))
-
-start = 0
-end = max_v - min_v
-while start <= end:
-    # 중간 지점
-    mid = (start + end) // 2
+start = 0               # 이 값이 왜 min_v이면 안됨?
+end = max_v - min_v     # 이 값은 무슨 의미를 갖는 거지?
+while start <= end:     # 분할 정복이 끝남을 의미
+    mid = (start + end) // 2    # 분할 정복을 위한 가운데를 의미
 
     suc = False
     for i in range(min_v, max_v+1):
-        if i <= board[0][0] <= i + mid and i <= board[-1][-1] <= i + mid:
+        if i <= board[0][0] <= i + mid and i <= board[-1][-1] <= i + mid:   # bfs탐색을 덜 하기 위한 조건
             check = bfs(i, i+mid)
             if check:
                 suc = True
