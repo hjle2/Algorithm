f7ab0c9379a3d290e607dcdc6b9db97d615c229d
WOW <62990408+hjle2@users.noreply.github.com>
2023-06-10 20:59:04 +0900

    [Platinum V] Title: 배열에서 이동, Time: 436 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Platinum/1981. 배열에서 이동/배열에서 이동.py
+++ 백준/Platinum/1981. 배열에서 이동/배열에서 이동.py
@@ -1,66 +1,49 @@
 from collections import deque
-import sys
- 
-input = sys.stdin.readline
-N = int(input())
-board = []
-MIN, MAX = 256, -1
-for _ in range(N):
-    inputs = list(map(int, input().split()))
-    for num in inputs:
-        MIN = min(MIN, num)
-        MAX = max(MAX, num)
-    board.append(inputs)
-s, e = board[0][0], board[N - 1][N - 1]
-dir = [[1, 0], [-1, 0], [0, 1], [0, -1]]
- 
- 
+
+n = int(input())
+board = [[*map(int, input().split())] for _ in range(n)]
+s, e = board[0][0], board[-1][-1]
+# 배열 내부의 가장 작은 값, 큰 값 구하기
+min_v, max_v = 1e9, 0
+for i in range(n):
+    min_v = min(min_v, min(board[i]))
+    max_v = max(max_v, max(board[i]))
+
+
+# 최대, 최소 값이 범위 내로 n-1, n-1에 도착할 수 있으면 1을 반환 아니라면 0을 반환하는 함수
+def bfs(left, right):
+    que = deque([(0, 0)])
+    v = [[False] * n for _ in range(n)]
+    v[0][0] = True
+
+    while que:
+        x, y = que.popleft()
+        if x == n - 1 and y == n - 1:
+            return True
+
+        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < n and not v[nx][ny] and left <= board[nx][ny] <= right:
+                v[nx][ny] = True
+                que.append((nx, ny))
+
+
 def check(mid):
-    condition = MAX + 1
-    for i in range(MIN, condition):
-        if not (i <= s <= i + mid and i <= e <= i + mid):
-            continue
-        if bfs(i, i + mid):
-            return True
+    for i in range(min_v, max_v + 1):
+        if i <= s <= i + mid and i <= e <= i + mid:
+            if bfs(i, i + mid):
+                return True
     return False
- 
- 
-def bfs(a, b):
-    visit = [[False for _ in range(N)] for _ in range(N)]
-    visit[0][0] = True
-    q = deque([(0, 0)])
-    while q:
-        hereY, hereX = q.popleft()
-        if hereY == N - 1 and hereX == N - 1:
-            return True
-        for d in dir:
-            thereY, thereX = hereY + d[0], hereX + d[1]
-            # 배열 범위를 벗어나면 패스
-            if thereY < 0 or thereY >= N or thereX < 0 or thereX >= N:
-                continue
-            # 방문했던 곳 패스
-            if visit[thereY][thereX]:
-                continue
-            num = board[thereY][thereX]
-            # 값 범위를 벗어나는 곳 패스
-            if not (a <= num <= b):
-                continue
-            visit[thereY][thereX] = True
-            q.append((thereY, thereX))
- 
- 
-def main():
-    left, right = 0, MAX
-    ans = 0
-    while left <= right:
-        mid = (left + right) // 2
-        if check(mid):
-            ans = mid
-            right = mid - 1
-        else:
-            left = mid + 1
-    print(ans)
- 
- 
-if __name__ == '__main__':
-    main()
+
+
+left = 0
+right = max_v
+while left <= right:
+    # 중간 지점
+    mid = (left + right) // 2
+
+    if check(mid):
+        right = mid - 1
+    else:
+        left = mid + 1
+print(right + 1)
