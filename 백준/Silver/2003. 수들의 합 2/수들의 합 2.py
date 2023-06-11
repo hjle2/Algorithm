b3e923966ea33bfd0cfead3ed8e2fb36a79de36f
WOW <62990408+hjle2@users.noreply.github.com>
2023-06-11 19:57:50 +0900

    [Silver IV] Title: 수들의 합 2, Time: 44 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,16 +1,15 @@
 n, m = map(int, input().split())
-ar = [*map(int, input().split())]
-
-s = [0] * (n+1)                 # 누적합을 계산해 둔 배열
-for i in range(1, n+1):
-    s[i] = s[i-1] + ar[i-1]
+ar = list(map(int, input().split()))
 
 ans = 0
+sum = 0
+end = 0
 
 for i in range(n):
-    for j in range(i, n):
-        tmp = s[j+1] - s[i]
-        if tmp == m:
-            ans += 1
-            break
+    while sum < m and end < n:  # 구간합이 m보다 작을 동안, end가 인덱스 범위내에 있을 동안만 반복
+        sum += ar[end]          # 구간합 구하기
+        end += 1                # 인덱스 증가시키기
+    if sum == m:
+        ans += 1
+    sum -= ar[i]                # 다음 i부터 구간합을 구하기위해 현재 값은 빼준다
 print(ans)
