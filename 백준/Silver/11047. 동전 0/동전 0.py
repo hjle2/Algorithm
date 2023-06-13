511ba73d77d95fca6b5a3798594af1d1234985c7
WOW <62990408+hjle2@users.noreply.github.com>
2023-06-13 21:28:26 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,12 +1,10 @@
 n, k = map(int, input().split())
-a = [int(input())for _ in range(n)]
-
+a = [int(input()) for _ in range(n)]
 
 ans = 0
 for i in range(n-1, -1, -1):
-    if a[i] > k: continue
-
-    cnt = k // a[i]
-    k -= cnt * a[i]
-    ans += cnt
+    if k < a[i]: continue
+    tmp = k // a[i]
+    ans += tmp
+    k -= tmp * a[i]
 print(ans)
