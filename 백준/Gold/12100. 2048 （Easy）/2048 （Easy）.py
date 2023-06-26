946e9c3bafe8f2cf21d9431ab7a91f9c94086b02
WOW <62990408+hjle2@users.noreply.github.com>
2023-06-26 17:05:21 +0900

    [Gold II] Title: 2048 (Easy), Time: 872 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -1,89 +1,95 @@
 from copy import deepcopy
 
-n = int(input())
-board = [[*map(int, input().split())]for _ in range(n)]
-
 
 def get_max(board):
-    ans = 0
+    ret = 0
     for i in range(n):
-        ans = max(ans, max(board[i]))
-    return ans
+        ret = max(ret, max(board[i]))
+    return ret
 
 
-def push(d, board): # d 방향으로 board를 민다
-    new_board = [[0]* n for _ in range(n)]
-    if d == 0: # 왼쪽으로 미는 경우
-        for r in range(n):
-            idx = 0
-            push_flag = False
-            for c in range(n):
-                if not board[r][c]: continue
-                if idx > 0 and new_board[r][idx-1] == board[r][c] and not push_flag:
-                    new_board[r][idx-1] += board[r][c]
-                    push_flag = True
-                    idx -= 1
-                else:
-                    new_board[r][idx] = board[r][c]
-                    push_flag = False
-                idx += 1
-
-    elif d == 1: # 오른쪽으로 미는 경우
-        for r in range(n):
-            idx = n-1
-            push_flag = False
-            for c in range(n-1, -1, -1):
-                if not board[r][c]: continue
-                if idx + 1 < n and new_board[r][idx+1] == board[r][c] and not push_flag:
-                    new_board[r][idx+1] += board[r][c]
-                    push_flag = True
-                    idx += 1
-                else:
-                    new_board[r][idx] = board[r][c]
-                    push_flag = False
-                idx -= 1
-    elif d == 2: # 위로 미는 경우
-        for c in range(n):
-            idx = 0
-            push_flag = False
-            for r in range(n):
-                if not board[r][c]: continue
-                if idx > 0 and new_board[idx-1][c] == board[r][c] and not push_flag:
-                    new_board[idx-1][c] += board[r][c]
-                    push_flag = True
-                    idx -= 1
-                else:
-                    new_board[idx][c] = board[r][c]
-                    push_flag = False
-                idx += 1
-    else: # 위로 미는 경우
-        for c in range(n):
-            idx = n-1
-            push_flag = False
-            for r in range(n-1, -1, -1):
-                if not board[r][c]: continue
-                if idx+1 < n and new_board[idx+1][c] == board[r][c] and not push_flag:
-                    new_board[idx+1][c] += board[r][c]
-                    push_flag = True
-                    idx += 1
-                else:
-                    new_board[idx][c] = board[r][c]
-                    push_flag = False
-                idx -= 1
-    return new_board
-
-
-def dfs(cnt, board):
-    global ans
-    if cnt == 5:
-        ans = max(ans, get_max(board))
+def dfs(direction, board, depth):
+    if depth == 5:
+        global ans
+        for i in range(n):
+            ans = max(ans, max(board[i]))
         return
+
+    ar = [[0] * n for _ in range(n)]
+
+    if direction == 0:
+        for i in range(n):
+            tmp = []
+            flag = False
+            for j in range(n):
+                if board[i][j]:
+                    if tmp and not flag and tmp[-1] == board[i][j]:
+                        tmp.append(board[i][j] + tmp.pop())
+                        flag = True
+                    else:
+                        tmp.append(board[i][j])
+                        flag = False
+            for j in range(n - len(tmp)):
+                tmp.append(0)
+            ar[i] = tmp
+    elif direction == 2:
+        for i in range(n):
+            tmp = []
+            flag = False
+            for j in range(n-1, -1, -1):
+                if board[i][j]:
+                    if tmp and not flag and tmp[-1] == board[i][j]:
+                        tmp.append(board[i][j] + tmp.pop())
+                        flag = True
+                    else:
+                        tmp.append(board[i][j])
+                        flag = False
+            for j in range(n - len(tmp)):
+                tmp.append( 0)
+            ar[i] = tmp[::-1]
+
+    elif direction == 1:
+        for i in range(n):
+            tmp = []
+            flag = False
+            for j in range(n):
+                if board[j][i]:
+                    if tmp and not flag and tmp[-1] == board[j][i]:
+                        tmp.append(board[j][i] + tmp.pop())
+                        flag = True
+                    else:
+                        tmp.append(board[j][i])
+                        flag = False
+            for j in range(n - len(tmp)):
+                tmp.append(0)
+            for j in range(n):
+                ar[j][i] = tmp[j]
+
+    elif direction == 3:
+        for i in range(n):
+            tmp = []
+            flag = False
+            for j in range(n-1, -1, -1):
+                if board[j][i]:
+                    if tmp and not flag and tmp[-1] == board[j][i]:
+                        tmp.append(board[j][i] + tmp.pop())
+                        flag = True
+                    else:
+                        tmp.append(board[j][i])
+                        flag = False
+            for j in range(n - len(tmp)):
+                tmp.append(0)
+            for j in range(n):
+                ar[j][i] = tmp[n-j-1]
+
     for d in range(4):
-        new_board = push(d, board)
-        dfs(cnt+1, new_board)
+        dfs(d, deepcopy(ar), depth + 1)
 
 
+n = int(input())
+ar = [[*map(int, input().split())]for _ in range(n)]
+d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 ans = 0
-dfs(0, board)
+for i in range(4):
+    dfs(i, deepcopy(ar), 0)
 print(ans)
-
