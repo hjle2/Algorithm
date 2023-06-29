ba81a296b923983c932199805473e138ff192c31
WOW <62990408+hjle2@users.noreply.github.com>
2023-06-29 20:31:37 +0900

    Update 냅색문제.py

--- 백준/Gold/1450. 냅색문제/냅색문제.py
+++ 백준/Gold/1450. 냅색문제/냅색문제.py
@@ -5,7 +5,7 @@ b_ar = ar[n//2:]
 a_sum = []
 b_sum = []
 
-
+# 이분 탐색으로 모든 합의 경우를 구하기
 def dfs(w_ar, s_ar, l, w):
     if l >= len(w_ar):
         s_ar.append(w)
@@ -13,7 +13,7 @@ def dfs(w_ar, s_ar, l, w):
     dfs(w_ar, s_ar, l+1, w)
     dfs(w_ar, s_ar, l+1, w + w_ar[l])
 
-
+# 이분 탐색으로 가방에 넣을 수 있는 경우의 수 구하기
 def binary_search(ar, target, l, r):
     while l < r:
         m = (l + r) // 2
