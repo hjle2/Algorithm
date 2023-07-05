88ce6905487a21ebc3a4328f6dec172dbc976d4e
WOW <62990408+hjle2@users.noreply.github.com>
2023-07-05 13:50:37 +0900

    [Gold II] Title: 컵라면, Time: 868 ms, Memory: 88996 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.py
+++ 백준/Gold/1781. 컵라면/컵라면.py
@@ -1,11 +1,22 @@
-import sys
-si = sys.stdin.readline
 import heapq
+import sys
+input = sys.stdin.readline
 n = int(input())
-prb = sorted([[*map(int, si().split())]for _ in range(n)])
-h = []
-for d, v in prb:
-  heapq.heappush(h, v)
-  while len(h) > d:
-    heapq.heappop(h)
-print(sum(h))
+cup = []
+last_deadline = 0
+for _ in range(n):
+    a, b = map(int, input().split())
+    last_deadline = max(last_deadline, a)
+    cup.append((a, b))
+cup.sort(key=lambda x:(-x[0], -x[1]))
+
+que = []
+idx, ans = 0, 0
+while last_deadline > 0:
+    while idx < n and cup[idx][0] >= last_deadline:
+        heapq.heappush(que, -cup[idx][1])
+        idx += 1
+    last_deadline -= 1
+    if que:
+        ans -= heapq.heappop(que)
+print(ans)
