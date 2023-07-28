d19d715f5336e3ed3bf00a1db94cfc0cf4757c0b
WOW <62990408+hjle2@users.noreply.github.com>
2023-07-28 21:30:55 +0900

    [Silver III] Title: 파도반 수열, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,11 @@
+def solve(n):
+    dp = [0, 1, 1, 1, 2, 2, 3, 4]
+    if n < len(dp):
+        print(dp[n])
+        return
+    for i in range(7, n):
+        dp.append(dp[-1] + dp[-5])
+    print(dp[n])
+
+for _ in range(int(input())):
+    solve(int(input()))
