a37490a2f93967dd72c826598bda00bab7dde8de
WOW <62990408+hjle2@users.noreply.github.com>
2023-08-23 13:39:15 +0900

    Update 메이즈러너.py

--- codetree/기출문제/메이즈러너.py
+++ codetree/기출문제/메이즈러너.py
@@ -4,6 +4,34 @@ people = []
 for _ in range(M):
     a, b = map(int, input().split())
     people.append((a-1, b-1))
+a, b = map(int, input().split())
+exit = (a-1, b-1)
 
-def turn(sx, sy, ex, ey):
+
+def get_min_square():
+    max_square = get_max_square()
+    
+    for square_size in range(1, max_square+1):
+        ret = is_possible(square_size)
+        if ret:
+            rotate_squre(ret[0], ret[1], ret[2])
+    
+    
+def get_max_square():
     pass
+
+
+def is_possible(square_size):
+    pass
+
+
+def rotate_squre(sx, sy, size):
+    # 회전
+    # 출구 위치 찾기
+    
+    # 내구도 감소
+    for x in range(sx, sx + size):
+        for y in range(sy, sy + size):
+            if board[x][y]:
+                board[x][y] -= 1
+    
