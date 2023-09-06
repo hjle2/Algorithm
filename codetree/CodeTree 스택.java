998fc410aff4464d29a992c03d69c4aa897d79d9
WOW <62990408+hjle2@users.noreply.github.com>
2023-09-06 19:50:34 +0900

    Update CodeTree 스택.java

--- codetree/CodeTree 스택.java
+++ codetree/CodeTree 스택.java
@@ -8,7 +8,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-
+        
         for (int i=0; i<N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String cmd = st.nextToken();
