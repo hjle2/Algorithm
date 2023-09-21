a26806827ac475af6ed57ebae1df0a9c8b952072
WOW <62990408+hjle2@users.noreply.github.com>
2023-09-21 14:43:11 +0900

    Update KMP.py

--- KMP.py
+++ KMP.py
@@ -12,19 +12,18 @@ def get_KMP_table(pattern):
             kmp_table[j] = i
     return kmp_table
 
-word = [*input().rstrip()]
-pattern = [*input().rstrip()]
-ans = []
-i = 0
-table = get_KMP_table(pattern)
-for j in range(len(word)):
-    while i > 0 and pattern[i] != word[j]:
-        i = table[i-1]
-        
-    if pattern[i] == word[j]:
-        i += 1
-        if i == len(pattern):
-            ans.append(j - i + 1)
-            i = table[i-1]
 
-print(table)
+def KMP(word, pattern):
+    ret = []
+    i = 0
+    table = get_KMP_table(pattern)
+    for j in range(len(word)):
+        while i > 0 and pattern[i] != word[j]:
+            i = table[i-1]
+    
+        if pattern[i] == word[j]:
+            i += 1
+            if i == len(pattern):
+                ret.append(j - i + 1)
+                i = table[i-1]
+    return ret
