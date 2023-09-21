4825826d9f2cb31a5f6b50cc4c941c65d688751b
WOW <62990408+hjle2@users.noreply.github.com>
2023-09-21 17:15:11 +0900

    Update KMP.py

--- KMP.py
+++ KMP.py
@@ -1,29 +1,32 @@
-def get_KMP_table(pattern):
-    length = len(pattern)
-    kmp_table = [0] * length
-
+def KMP(word, pattern):
+    n = len(pattern)
+    
+    # table 만들기
+    table = [0] * n
     i = 0
-    for j in range(1, length):
-        if i > 0 and pattern[i] != pattern[j]:
-            i = kmp_table[i-1]
+    for j in range(n):
+        while i > 0 and pattern[i] != pattern[j]:
+            i = table[i-1]
 
         if pattern[i] == pattern[j]:
             i += 1
-            kmp_table[j] = i
-    return kmp_table
+            table[j] = i
+    # <<
 
-
-def KMP(word, pattern):
+    # KMP 알고리즘을 이용하여 패턴 찾기
     ret = []
+    n = len(word)
     i = 0
-    table = get_KMP_table(pattern)
-    for j in range(len(word)):
+    for j in range(n):
         while i > 0 and pattern[i] != word[j]:
             i = table[i-1]
-    
+        
         if pattern[i] == word[j]:
             i += 1
             if i == len(pattern):
                 ret.append(j - i + 1)
                 i = table[i-1]
+    # <<
     return ret
+    
+    
