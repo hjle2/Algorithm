81d286ad3156f22c351b354b9cd7a26b3f6294f8
WOW <62990408+hjle2@users.noreply.github.com>
2023-11-06 17:29:33 +0900

    Update KMP.py

--- KMP.py
+++ KMP.py
@@ -6,7 +6,7 @@ def KMP(full_string, pattern):
     for j in range(1, n_p):
         while i > 0 and pattern[i] != pattern[j]:
             i = table[i-1]
-
+ 
         if pattern[i] == pattern[j]:
             i += 1
             table[j] = i
