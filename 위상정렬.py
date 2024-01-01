56f4a1b2c311ccfa97c7639692e37bd0bf4e340b
WOW <62990408+hjle2@users.noreply.github.com>
2024-01-01 16:46:12 +0900

    Update 위상정렬.py

--- 위상정렬.py
+++ 위상정렬.py
@@ -12,7 +12,7 @@ graph = [[] for _ in range(v + 1)]
 for _ in range(e):
     a, b = map(int, input().split())
     graph[a].append(b)
-    indegree[b] += 1 
+    indegree[b] += 1
 
 
 def topology_sort():
